74e5a537f7570c9306957026c91661798fc2cc82
Maloochow <luyao.mly@gmail.com>
2020-08-18 15:03:04 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,16 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  # def posts_index
+  #   # @posts = Author.includes(:posts).where(id: params[:id])
+  #   @author = Author.find(params[:id])
+  #   @posts = @author.posts
+  #   render template: 'posts/index'
+  # end
+
+  # def post
+  #   @post = Post.find(params[:post_id])
+  #   render :'posts/show'
+  # end
+
 end
